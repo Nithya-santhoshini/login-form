7e574fcfdb6a1b4a8f1297810c481fdf85fcd04b
Nithya-Santhoshini <santhupothuri@gmail.com>
2023-08-30 13:45:04 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [data,setData] =useState({
+        username:"",
+        password:""
+    })
+    const {username,password} =data;
+    const changehandler = e => {
+        setData({...data,[e.target.name]:[e.target.value]})
+    }
+    const submitHandler = e => {
+        e.preventDefault()
+        console.log(data)
+    }
+    return(
+        <div>
+            <center>
+                <form onSubmit={submitHandler} >
+                    <h1>login form</h1>
+                    <label>username</label> <br /> <br />
+                    <input type="text" name="username" value={username} onChange={changehandler} /> <br />
+                    <label>password</label> <br/>
+                    <input type="password  "name="password" value={password} onChange={changehandler}/> <br />
+                    <button>submit</button>
+                </form>
+            </center>
+        </div>
+    )
 }
-
-export default App;
+export default App
