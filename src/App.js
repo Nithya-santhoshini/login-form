bbb9551657d16aca8e7aec8c11900628def9d556
Nithya-Santhoshini <santhupothuri@gmail.com>
2023-08-30 14:08:08 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import './App.css';
 const App = () => {
     const [data,setData] =useState({
         username:"",
-        password:""
+        email:"",
+        password:"",
+        confirmpassword:"",
+
+
     })
-    const {username,password} =data;
+    const {username,email,password,confirmpassword} =data;
     const changehandler = e => {
-        setData({...data,[e.target.name]:[e.target.value]})
+        setData({...data,[e.target.name]:e.target.value})
     }
     const submitHandler = e => {
         e.preventDefault()
-        console.log(data)
+        if(password === confirmpassword){
+            console.log("password are matching");
+        }
+        else{
+            console.log("password are not matching")
+        }
+        
     }
     return(
         <div>
@@ -21,8 +31,12 @@ const App = () => {
                     <h1>login form</h1>
                     <label>username</label> <br /> <br />
                     <input type="text" name="username" value={username} onChange={changehandler} /> <br />
+                    <label>email</label> <br />
+                    <input type="text" name="email" value={email} onChange={changehandler} /> <br />
                     <label>password</label> <br/>
                     <input type="password  "name="password" value={password} onChange={changehandler}/> <br />
+                    <label>confirmpassword</label> <br/>
+                    <input type="confirmpassword  "name="confirmpassword" value={confirmpassword} onChange={changehandler}/> <br />
                     <button>submit</button>
                 </form>
             </center>
